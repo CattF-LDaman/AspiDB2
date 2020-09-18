9e3c67883f4451e383ab116950ef657af7c4a824
CattF <pycatt@gmail.com>
2020-09-18 16:52:04 +0200

    Test + extra logging

--- Aspi2/database.py
+++ Aspi2/database.py
@@ -14,7 +14,7 @@ def build(location,name,struc,slots=1024,max_key_length=64,index_size_bytes=12):
 
         f.write(MAGIC_NUM)
 
-        f.write(len(name.encode('utf-8')))
+        f.write(len(name.encode('utf-8')).to_bytes(2,"little"))
         f.write(name.encode('utf-8'))
 
         f.write(VERSION.to_bytes(3,'little'))
@@ -59,7 +59,7 @@ class Database:
 
                 raise WrongMagicNum("Didn't find magic number at beginning of file at location")
 
-            dbn_len = db_f.read(4)
+            dbn_len = db_f.read(2)
             self.name = db_f.read(int.from_bytes(dbn_len,'little')).decode('utf-8')
 
             self.version = int.from_bytes(db_f.read(3),'little')
@@ -89,7 +89,7 @@ class Database:
         self.logger = logging_utils.Logger(self.name, self.config['logger_directory'], self.config["logger_enabled"], self.config["logger_print_enabled"], self.config["logger_print_level"])
         self.log = self.logger.log
 
-        self.log(f"Database ' {self.name} ' initialised with version ' {self.version} ' (Client version is ' {VERSION} '),  structure of size {len(self.structure)} ( {len(self.structure/1024/1024)} mb ), at most {int(2**(self.indexsize*8)/self.entry_size)} entries possible.")
+        self.log(f"Database ' {self.name} ' initialised with version ' {self.version} ' (Client version is ' {VERSION} '),  structure of size {len(self.structure)} ( {len(self.structure)/1024/1024} mb ), at most {int(2**(self.indexsize*8)/self.entry_size)} entries possible.")
 
     def get_slot(self,key):
 
@@ -97,7 +97,7 @@ class Database:
 
     def get_slot_index(self,key):
 
-        return self.indices_location + get_slot(key) * self._slotsize
+        return self.indices_location + self.get_slot(key) * self._slotsize
 
 class Accessor:
 
@@ -113,17 +113,97 @@ class Accessor:
 
         self._file.seek(self.db.data_location+index)
 
-        self._file.write(OCCUPANCE_OCCUPIED if not collided else OCCUPANCE_OCCUPIED_COLLIDED)
+        self._file.write(OCCUPANCE_OCCUPIED if not collider else OCCUPANCE_OCCUPIED_COLLIDED)
         self._file.write(collider.to_bytes(self.db.indexsize,'little') if collider else b"\x00"*self.db.indexsize)
         self._file.write(len(key).to_bytes(self.db.keysize_bytesize,'little'))
         self._file.write(key.encode("ascii")+(self.db.keysize-len(key))*b"\x00")
         self._file.write(data)
 
+    def delete(self,key):
+
+        self.db.log(f"DELETE {key}")
+
+        slot_index = self.db.get_slot_index(key)
+
+        self._file.seek(slot_index)
+        occupance_info = self._file.read(1)
+        self._file.seek(slot_index)
+
+        if occupance_info == OCCUPANCE_NOT_OCCUPIED:
+
+            return None
+
+        elif occupance_info == OCCUPANCE_OCCUPIED:
+
+            self._file.seek(1,1)
+
+            o_dataindex = int.from_bytes(self._file.read(self.db.indexsize),'little')
+
+            self._file.seek(self.db.data_location+o_dataindex)
+
+            data_occupance_info =  self._file.read(1)
+
+            if data_occupance_info == OCCUPANCE_NOT_OCCUPIED:
+
+                self._file.seek(self.db.data_location+cur_dataindex)
+
+            elif data_occupance_info in [OCCUPANCE_OCCUPIED_COLLIDED,OCCUPANCE_OCCUPIED,OCCUPANCE_NOT_OCCUPIED_COLLIDED]:
+
+                cur_dataindex = o_dataindex
+
+                while True:
+
+                    self._file.seek(self.db.data_location+cur_dataindex)
+
+                    data_occupance_info =  self._file.read(1)
+
+                    collided_index = int.from_bytes(self._file.read(self.db.indexsize),'little')
+
+                    key_r_len = int.from_bytes(self._file.read(self.db.keysize_bytesize),'little')
+                    key_r = self._file.read(self.db.keysize)[:key_r_len].decode('ascii')
+
+                    if key_r == key:
+
+                        if data_occupance_info in [OCCUPANCE_OCCUPIED,OCCUPANCE_OCCUPIED_COLLIDED]:
+
+                            self._file.seek(self.db.data_location+cur_dataindex)
+                            self._file.write({OCCUPANCE_OCCUPIED:OCCUPANCE_NOT_OCCUPIED,OCCUPANCE_OCCUPIED_COLLIDED:OCCUPANCE_NOT_OCCUPIED_COLLIDED}[data_occupance_info])
+                            break
+
+                        else:
+
+                            raise ValueError("Couldn't find key that you were trying to delete")
+
+                    else:
+
+                        if data_occupance_info == OCCUPANCE_OCCUPIED_COLLIDED:
+
+                            cur_dataindex = collided_index
+
+                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED:
+
+                            raise ValueError("Couldn't find key that you were trying to delete")
+
+                        elif data_occupance_info == OCCUPANCE_OCCUPIED:
+
+                            raise ValueError("Couldn't find key that you were trying to delete")
+
+                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED_COLLIDED:
+
+                            if collided_index == o_dataindex: # To evade infinite looping
+
+                                raise ValueError("Couldn't find key that you were trying to delete")
+
+                            cur_dataindex = collided_index
+
+
     def set(self,key,data):
+
+        self.db.log(f"SET {key}")
 
         comp_data = self.db.structure.compile(data)
 
-        slot_index = self.get_slot_index(key)
+        slot_index = self.db.get_slot_index(key)
 
         self._file.seek(slot_index)
         occupance_info = self._file.read(1)
@@ -142,7 +222,7 @@ class Accessor:
 
             self._file.seek(1,1)
 
-            o_dataindex = self._file.read(self.db.indexsize)
+            o_dataindex = int.from_bytes(self._file.read(self.db.indexsize),'little')
 
             self._file.seek(self.db.data_location+o_dataindex)
 
@@ -152,7 +232,7 @@ class Accessor:
 
                 self._write_data_at(o_dataindex,key,comp_data)
 
-            elif data_occupance_info in [OCCUPANCE_OCCUPIED_COLLIDED,OCCUPANCE_NOT_OCCUPIED_COLLIDED]:
+            elif data_occupance_info in [OCCUPANCE_OCCUPIED_COLLIDED,OCCUPANCE_OCCUPIED,OCCUPANCE_NOT_OCCUPIED_COLLIDED]:
 
                 cur_dataindex = o_dataindex
 
@@ -168,9 +248,9 @@ class Accessor:
                     collided_index = int.from_bytes(self._file.read(self.db.indexsize),'little')
 
                     key_r_len = int.from_bytes(self._file.read(self.db.keysize_bytesize),'little')
-                    key_r = self._file.read(self.db.keysize[:key_r])
+                    key_r = self._file.read(self.db.keysize)[:key_r_len].decode('utf-8')
 
-                    if key_r = key:
+                    if key_r == key:
 
                         self._write_data_at(cur_dataindex,key,comp_data)
                         break
@@ -191,13 +271,13 @@ class Accessor:
                             new_dataindex = int(os.path.getsize(self.db.location))-self.db.data_location if not potential_index else potential_index
 
                             self._file.seek(self.db.data_location+cur_dataindex)
-                            sel._file.write(OCCUPANCE_OCCUPIED_COLLIDED)
+                            self._file.write(OCCUPANCE_OCCUPIED_COLLIDED)
                             self._file.write(new_dataindex.to_bytes(self.db.indexsize,"little"))
 
                             self._write_data_at(new_dataindex,key,comp_data,None if not potential_index else potential_index_original_collider)
                             break
 
-                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED_COLLIDED
+                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED_COLLIDED:
 
                             potential_index = cur_dataindex
                             potential_index_original_collider = collided_index
@@ -211,7 +291,9 @@ class Accessor:
 
     def get(self,key):
 
-        slot_index = self.get_slot_index(key)
+        self.db.log(f"GET {key}")
+
+        slot_index = self.db.get_slot_index(key)
 
         self._file.seek(slot_index)
         occupance_info = self._file.read(1)
@@ -225,7 +307,7 @@ class Accessor:
 
             self._file.seek(1,1)
 
-            o_dataindex = self._file.read(self.db.indexsize)
+            o_dataindex = int.from_bytes(self._file.read(self.db.indexsize),'little')
 
             self._file.seek(self.db.data_location+o_dataindex)
 
@@ -235,7 +317,7 @@ class Accessor:
 
                 return None
 
-            elif data_occupance_info in [OCCUPANCE_OCCUPIED_COLLIDED,OCCUPANCE_NOT_OCCUPIED_COLLIDED]:
+            elif data_occupance_info in [OCCUPANCE_OCCUPIED_COLLIDED,OCCUPANCE_OCCUPIED,OCCUPANCE_NOT_OCCUPIED_COLLIDED]:
 
                 cur_dataindex = o_dataindex
 
@@ -248,9 +330,9 @@ class Accessor:
                     collided_index = int.from_bytes(self._file.read(self.db.indexsize),'little')
 
                     key_r_len = int.from_bytes(self._file.read(self.db.keysize_bytesize),'little')
-                    key_r = self._file.read(self.db.keysize[:key_r])
+                    key_r = self._file.read(self.db.keysize)[:key_r_len].decode('ascii')
 
-                    if key_r = key:
+                    if key_r == key:
 
                         if data_occupance_info in [OCCUPANCE_OCCUPIED,OCCUPANCE_OCCUPIED_COLLIDED]:
 
@@ -274,7 +356,7 @@ class Accessor:
 
                             return None
 
-                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED_COLLIDED
+                        elif data_occupance_info == OCCUPANCE_NOT_OCCUPIED_COLLIDED:
 
                             if collided_index == o_dataindex: # To evade infinite looping
 
--- test.py
+++ test.py
@@ -2,4 +2,22 @@ from Aspi2 import database
 
 def test():
 
-    db = database.Database()
+    test_struc = (("firstname","UnicodeString",{"size":64},False),("lastname","UnicodeString",{"size":128},False),("age","IntUnsigned",{"bitsize":8},True))
+
+    database.build("TestDB.asp2", "TestDB", test_struc)
+
+    db = database.Database("TestDB.asp2")
+
+    db_accessor  = database.Accessor(db)
+
+    db_accessor.set("jsmith",{"firstname":"John","lastname":"Smith","age":35})
+
+    print(db_accessor.get("jsmith"))
+
+    db_accessor.delete("jsmith")
+
+    print(db_accessor.get("jsmith"))
+
+if __name__ == "__main__":
+
+    test()
