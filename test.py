3c1f5d97c4e806a30443e36e5119fac70d65d3fc
Luwe Daman <luwedm@gmail.com>
2020-09-20 17:20:12 +0200

    Threaded test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from Aspi2 import database
 
+import threading
+import random
+
 def test():
 
     test_struc = (("firstname","UnicodeString",{"size":64},False),("lastname","UnicodeString",{"size":128},False),("age","IntUnsigned",{"bitsize":8},True))
@@ -36,6 +39,40 @@ def find_test():
 
     print(db_accessor.find("firstname", "has", "Paul"))
 
+def threaded_test():
+
+        test_struc = (("firstname","UnicodeString",{"size":64},False),("lastname","UnicodeString",{"size":128},False),("age","IntUnsigned",{"bitsize":8},True))
+
+        database.build("TestDB.asp2", "TestDB", test_struc)
+
+        db = database.Database("TestDB.asp2")
+
+        def _dbthread():
+
+            thr_accessor = database.Accessor(db)
+
+            c = [{"firstname":"John","lastname":"Smith","age":35},{"firstname":"Elton","lastname":"John","age":72},{"firstname":"Sir Paul","lastname":"Mccartney","age":78}]
+
+            for _ in range(1000):
+
+                thr_accessor.set("person", random.choice(c))
+
+        th1 = threading.Thread(target=_dbthread)
+        th2 = threading.Thread(target=_dbthread)
+        th3 = threading.Thread(target=_dbthread)
+
+        th1.start()
+        th2.start()
+        th3.start()
+        th3.join()
+        th2.join()
+        th1.join()
+
+        print(database.Accessor(db).get("person"))
+
+
+
+
 if __name__ == "__main__":
 
-    find_test()
+    threaded_test()
